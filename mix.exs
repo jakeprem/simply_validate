fd36e692fa540b434f3b388d90cd6a6a1fac5766
Jake Prem <jakeprem@gmail.com>
2019-10-10 15:38:15 -0400

    Update mix.exs for publishing

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule SimplyValidate.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  def package do
+    [
+      description: "A simple library for validating anything",
+      licenses: ["MIT"],
+      links: []
     ]
   end
 
@@ -21,8 +30,7 @@ defmodule SimplyValidate.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
