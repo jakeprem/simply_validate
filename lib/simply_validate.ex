090b84fa97fa0402587ee8eda0e9594778ac07d5
Jake Prem <jakeprem@gmail.com>
2019-10-10 15:20:28 -0400

    Initial commit

---
+++ lib/simply_validate.ex
@@ -0,0 +1,18 @@
+defmodule SimplyValidate do
+  @moduledoc """
+  Documentation for SimplyValidate.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimplyValidate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simply_validate_test.exs
@@ -0,0 +1,8 @@
+defmodule SimplyValidateTest do
+  use ExUnit.Case
+  doctest SimplyValidate
+
+  test "greets the world" do
+    assert SimplyValidate.hello() == :world
+  end
+end
