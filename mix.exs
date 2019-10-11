42ce304c2b9da01c4263ab9005040c27d6727130
Jake Prem <jakeprem@gmail.com>
2019-10-11 09:18:39 -0400

    Change minimum elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SimplyValidate.MixProject do
     [
       app: :simply_validate,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package()
